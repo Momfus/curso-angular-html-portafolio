9e43ba9a1227411c498aa5b5740003d5c21ed242
Momfus <momfus@outlook.com>
2019-11-11 16:03:31 -0300

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -12,18 +12,42 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada: boolean = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
     console.log('Servicio on');
 
-    // Leeer archivo JSON
-    this.http.get( 'assets/data/data-pagina.json' )
-            .subscribe( (res: InfoPagina) => {
-
-              this.cargada = true;
-              this.info = res;
-
-            } )  ;
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
+
+  private cargarInfo() {
+
+     // Leeer archivo JSON
+     this.http.get( 'assets/data/data-pagina.json' )
+             .subscribe( (res: InfoPagina) => {
+
+               this.cargada = true;
+               this.info = res;
+
+             })  ;
+
+  }
+
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-curso-f22bc.firebaseio.com/equipo.json')
+            .subscribe( (res: any[] ) => {
+
+              this.equipo = res;
+              console.log(res);
+
+
+            });
+
+  }
+
 }
