220c68054e3943a72ec09a9d6671eb50c7385999
Momfus <momfus@outlook.com>
2019-11-11 15:33:04 -0300

    Fin seccion 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  // tslint:disable: no-inferrable-types
+
+  info: InfoPagina = {};
+  cargada: boolean = false;
+
+  constructor( private http: HttpClient ) {
+
+    console.log('Servicio on');
+
+    // Leeer archivo JSON
+    this.http.get( 'assets/data/data-pagina.json' )
+            .subscribe( (res: InfoPagina) => {
+
+              this.cargada = true;
+              this.info = res;
+
+            } )  ;
+
+  }
+}
